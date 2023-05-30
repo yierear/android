8fa937d44586688eabfdc44f31500542bd38238a
christyeeee <106256035+christyeeee@users.noreply.github.com>
2023-05-30 21:21:35 +0800

    home添加轮播+完善列表文字

--- finnal/app/src/main/java/com/example/finnal/home.java
+++ finnal/app/src/main/java/com/example/finnal/home.java
@@ -3,14 +3,24 @@ package com.example.finnal;
 //推荐文本库--->改成数据库
 public class home {
 
-    private static String[] names={"电影院","活动室","自习室1","自习室2",
-            "研讨室1","研讨室2","开发室"};
-    private static String[] news={"配备投影仪", "空旷场地", "安静","安静",
-            "隔音", "隔音", "带电脑"};
+    private static String[] names={"电影院","活动室","自习室 ","研讨室","开发室"};
+    private static String[] news={"内部设施：投影仪，椅子若干，麦克风\n"+
+            "最大容纳人数：30人\n"+
+            "适宜活动：组织分享会，观赏电影",
+            "内部设施：桌子4张，壁挂白板2张\n" +
+                    "最大容纳人数：14人\n" +
+                    "适宜活动：自习，小声讨论\n",
+            "内部设施：桌椅若干，丰富藏书\n"+
+            "最大容纳人数：40人\n"+
+            "适宜活动：自习、借阅图书",
+            "内部设施：会议桌，椅子若干\n"+
+            "最大容纳人数：20人\n"+
+            "适宜活动：小组讨论",
+            "内部设施：桌椅、电脑、白板\n"+
+            "最大容纳人数：10人\n"+
+            "适宜活动：借用电脑解决相关问题"};
 
-    private static int[] icons={R.drawable.cinema,R.drawable.activityroom,
-            R.drawable.selfstudyroom,R.drawable.selfstudyroom,
-            R.drawable.seminarroom,R.drawable.seminarroom,R.drawable.developmentroom};
+    private static int[] icons={R.drawable.cinema,R.drawable.activityroom,R.drawable.selfstudyroom,R.drawable.seminarroom,R.drawable.developmentroom};
 
     public static String[] getNames() {
         return names;
