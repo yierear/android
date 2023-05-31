2b68ee9c71b6dc10b02885504f32c4d4289b3087
yierear <1392228657@qq.com>
2023-05-31 11:44:06 +0800

    database
    修改了数据库名称和列表名

--- finnal/app/src/main/java/com/example/finnal/DBHelper.java
+++ finnal/app/src/main/java/com/example/finnal/DBHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 //创建数据库
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "fruit.db";
+    public static final String DATABASE_NAME = "lib.db";
 
     private static final int DATABASE_VERSION = 1;
 
@@ -16,8 +16,8 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     final String CREATE_TABLE_SQL1="create table if not exists user(_id integer primary key autoincrement ,username,password)";
-    final String CREATE_TABLE_SQL2="create table if not exists pinglun(_id integer primary key autoincrement ,userid,pinglun)";
-    final String CREATE_TABLE_SQL3="create table if not exists yuyue(_id integer primary key autoincrement ,userid,pinzhong,data)";
+    final String CREATE_TABLE_SQL2="create table if not exists comment(_id integer primary key autoincrement ,userid,comment)";
+    final String CREATE_TABLE_SQL3="create table if not exists reserve(_id integer primary key autoincrement ,userid,type,data)";
     public void onCreate(SQLiteDatabase sqLitedatabase){
         sqLitedatabase.execSQL(CREATE_TABLE_SQL1);
         sqLitedatabase.execSQL(CREATE_TABLE_SQL2);
